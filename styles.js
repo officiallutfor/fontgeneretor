a335ea92ce748648ed74647f54597572cf59c7e6
officiallutfor <123925560+officiallutfor@users.noreply.github.com>
2023-01-30 12:17:17 +0600

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,426 @@
+// Categories Goes Here
+categories.push({
+	id: 1,
+	title: "Fancy Text Styles",
+});
+
+categories.push({
+	id: 2,
+	title: "Latin Mix Char Styles",
+	ad_before: false,
+	ad_code: '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> <!-- STM AD 2 --> <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2276097048480329" data-ad-slot="4852600760" data-ad-format="auto" data-full-width-responsive="true"></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>',
+});
+
+categories.push({
+	id: 3,
+	title: "Stylish Sides Texts",
+});
+
+categories.push({
+	id: 4,
+	title: "Decorated Stylish Text",
+	ad_before: false,
+	ad_code: '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> <!-- STM Ad 3 --> <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2276097048480329" data-ad-slot="7351922975" data-ad-format="auto" data-full-width-responsive="true"></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>',
+});
+
+categories.push({
+	id: 5,
+	title: "Joined with Symbols Text",
+});
+
+categories.push({
+	id: 6,
+	title: "Text with Emoticons",
+});
+
+categories.push({
+	id: 7,
+	title: "Text with Emojis",
+});
+
+// Styles Goes Here
+// CATEGORY 1 (Fancy Fonts)
+styles.push({
+	name: "script",
+	title: "Script Text",
+	category: 1
+});
+
+styles.push({
+	name: "script_bold",
+	title: "Script Bold Text",
+	category: 1
+});
+
+styles.push({
+	name: "fullsquare",
+	title: "Squared Text 1",
+	category: 1
+});
+
+styles.push({
+	name: "double_struck",
+	title: "Double-Struck Text",
+	category: 1
+});
+
+styles.push({
+	name: "circled",
+	title: "Circled Text",
+	category: 1
+});
+
+styles.push({
+	name: "gothic",
+	title: "Gothic Text",
+	category: 1
+});
+
+styles.push({
+	name: "gothic_bold",
+	title: "Gothic Bold Text",
+	category: 1
+});
+
+styles.push({
+	name: "squared",
+	title: "Squared Text 2",
+	category: 1
+});
+
+styles.push({
+	name: "monospace",
+	title: "Monospace Text",
+	category: 1
+});
+
+styles.push({
+	name: "bold",
+	title: "Bold Text",
+	category: 1
+});
+styles.push({
+	name: "italic",
+	title: "Italic Text",
+	category: 1
+});
+
+styles.push({
+	name: "sans",
+	title: "Sans-Serif Text",
+	category: 1
+});
+
+styles.push({
+	name: "sansitalic",
+	title: "Sans-Serif Italic",
+	category: 1
+});
+
+styles.push({
+	name: "bold_italic",
+	title: "Sans-Serif Bold Italic",
+	category: 1,
+	ad_before: true,
+	ad_code: '<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> <!-- STM AD 2 --> <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-2276097048480329" data-ad-slot="4852600760" data-ad-format="auto" data-full-width-responsive="true"></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>',
+});
+
+styles.push({
+	name: "blackcircle",
+	title: "Black Circled Text",
+	category: 1
+});
+
+styles.push({
+	name: "wide",
+	title: "Full-Width Text (Wide)",
+	category: 1
+});
+
+styles.push({
+	name: "very_tiny",
+	title: "Tiny Text",
+	category: 1
+});
+
+styles.push({
+	name: "parenthesis",
+	title: "Parenthesized Text",
+	category: 1
+});
+
+styles.push({
+	name: "underline",
+	title: "Text with Underline",
+	category: 1
+});
+
+styles.push({
+	name: "flip",
+	title: "Upside-Down Text (Flip)",
+	category: 1
+});
+
+// CATEGORY 2 (Latin Mix Text)
+
+styles.push({
+	name: "small",
+	title: "Latin Small Capital Text",
+	category: 2
+});
+
+styles.push({
+	name: "latinmix1",
+	title: "Latin Mix 1",
+	category: 2
+});
+
+styles.push({
+	name: "latinmix2",
+	title: "Latin Mix 2",
+	category: 2
+});
+
+styles.push({
+	name: "latinmix3",
+	title: "Latin Mix 3",
+	category: 2
+});
+
+styles.push({
+	name: "gothiclatin",
+	title: "Gothic+Latin Mix",
+	category: 2
+});
+
+styles.push({
+	name: "latinmix4",
+	title: "Latin Mix 4",
+	category: 2
+});
+
+styles.push({
+	name: "latinmix5",
+	title: "Latin Mix 5",
+	category: 2
+});
+
+// CATEGORY 3 (Boxed Text Styles)
+
+styles.push({
+	name: "brack",
+	title: "Text with Lenticular Bracket ",
+	category: 3
+});
+
+styles.push({
+	name: "brack2",
+	title: "Text with White Corner Brackets ",
+	category: 3
+});
+
+styles.push({
+	name: "brack3",
+	title: "Text with Square Tick Brackets ",
+	category: 3
+});
+
+styles.push({
+	name: "brack4",
+	title: "Text with Heavy Pointing Brackets",
+	category: 3
+});
+
+styles.push({
+	name: "brack5",
+	title: "Text with Angle Dot Brackets",
+	category: 3
+});
+
+styles.push({
+	name: "shade",
+	title: "Text with Shade Sides",
+	category: 3
+});
+
+styles.push({
+	name: "brack6",
+	title: "Text with Curly Brackets",
+	category: 3
+});
+
+styles.push({
+	name: "wiggly",
+	title: "Text with Double Wiggly Sides",
+	category: 3
+});
+
+// CATEGORY 4 (Decorated Stylish Text)
+
+styles.push({
+	name: "block",
+	title: "Text with Blocks",
+	category: 4
+});
+
+styles.push({
+	name: "stars",
+	title: "Stars Decorated Text",
+	category: 4
+});
+
+styles.push({
+	name: "shades3",
+	title: "Text with Shades",
+	category: 4
+});
+
+styles.push({
+	name: "squarequad",
+	title: "Square Text with Quadrants",
+	category: 4
+});
+
+styles.push({
+	name: "decor",
+	title: "Sides Decorated Text",
+	category: 4
+});
+
+styles.push({
+	name: "block2",
+	title: "Text with Blocks 2",
+	category: 4
+});
+
+styles.push({
+	name: "decor2",
+	title: "Text with Up Sides",
+	category: 4
+});
+
+styles.push({
+	name: "widebrack",
+	title: "Wide Text with Brackets",
+	category: 4
+});
+
+styles.push({
+	name: "decorscript",
+	title: "Decorated Script Text",
+	category: 4
+});
+
+// CATEGORY 5 (Joined with Symbols)
+
+styles.push({
+	name: "heart",
+	title: "Text with Hearts",
+	category: 5
+});
+
+styles.push({
+	name: "star",
+	title: "Star Symbols Text",
+	category: 5
+});
+
+styles.push({
+	name: "wave",
+	title: "Text with Waves",
+	category: 5
+});
+
+styles.push({
+	name: "arrowdots",
+	title: "Text with Arrows",
+	category: 5
+});
+
+styles.push({
+	name: "smiletext",
+	title: "Text with Smiley",
+	category: 5
+});
+
+styles.push({
+	name: "zigzagarrow",
+	title: "Text with Zig Zag Arrows",
+	category: 5
+});
+
+// CATEGORY 6 (Text with Emoticons)
+
+styles.push({
+	name: "smile1",
+	title: "Smile with Upper Eyes Text",
+	category: 6
+});
+
+styles.push({
+	name: "smile2",
+	title: "Cute Smile Text",
+	category: 6
+});
+
+styles.push({
+	name: "confuse",
+	title: "Confused Emoticon Text",
+	category: 6
+});
+
+styles.push({
+	name: "smile3",
+	title: "Smile with Smiley Eyes Text",
+	category: 6
+});
+
+styles.push({
+	name: "happy1",
+	title: "Happy Emoticon Text",
+	category: 6
+});
+
+styles.push({
+	name: "happycute",
+	title: "Cute Happy Emoticon Text",
+	category: 6
+});
+
+// CATEGORY 7 (Text with Emojis)
+
+styles.push({
+	name: "flowers1",
+	title: "Script Text with Flowers",
+	category: 7
+});
+
+styles.push({
+	name: "flowers2",
+	title: "Script Text with Flowers 2",
+	category: 7
+});
+
+styles.push({
+	name: "flowers3",
+	title: "Bold Script Text with Leaves",
+	category: 7
+});
+
+styles.push({
+	name: "wavemoji",
+	title: "Italic Text with Waves",
+	category: 7
+});
+
+styles.push({
+	name: "smilesun",
+	title: "Monospace Text with Smiling Sun",
+	category: 7
+});
+
+styles.push({
+	name: "gstars",
+	title: "Double Struck Text with Stars",
+	category: 7
+});
